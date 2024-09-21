f33fe6f42f6d46131a1b5a5d22e89f178bb6ea56
Hryshankou Vitali <AsperinDe402526@gmail.com>
2024-09-21 17:27:08 +0200

    Создан класс ColumnByte

---
+++ pinksqlit/src/main/java/com/example/pinksqlit/column/type/ColumnByte.kt
@@ -0,0 +1,23 @@
+package com.example.pinksqlit.column.type
+
+import android.database.Cursor
+import com.example.pinksqlit.column.Autoincrement
+import com.example.pinksqlit.column.ColumnSqlit
+import com.example.pinksqlit.column.PrimaryKey
+import com.example.pinksqlit.column.SqlitType
+import com.example.pinksqlit.model.TableName
+import com.example.pinksqlit.model.Unique
+
+class ColumnByte<E>(
+    override val position: Int,
+    override val mapValue: (entity: E) -> Byte?,
+    override val tableName: TableName,
+    override val columnName: String,
+) : ColumnSqlit<E, Byte>() {
+    override val sqlitType: SqlitType = SqlitType.INTEGER
+    override val primaryKey: PrimaryKey = PrimaryKey.No
+    override val autoincrement: Autoincrement = Autoincrement.No
+    override val unique: Unique = Unique.No
+    override fun getValue(cursor: Cursor?): Byte? = cursor?.getInt(position)?.toByte()
+    override fun valueToString(velue: Byte?): String = velue.toString()
+}
